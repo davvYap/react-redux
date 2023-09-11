5bcce893e1da85b97e9b32a7a71ed132f3054236
David Yap <davvyap@gmail.com>
2023-09-11 21:55:49 +0800

    toggle button state & payload in dispatch action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   // auto subscribe and un-subscribe to the store state
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const dispatch = useDispatch();
 
@@ -14,12 +15,15 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+
       <div>
         <button onClick={incrementHandler}>Increase</button>
         <button onClick={decrementHandler}>Decrease</button>
